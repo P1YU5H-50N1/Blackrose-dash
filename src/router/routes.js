22a0062472ef96e92f7d8a85118582d864184674
PiyushAtHK <piyush.soni@brightlifecare.com>
2023-04-23 17:01:50 +0530

    Auth routers

---
+++ src/router/routes.js
@@ -0,0 +1,91 @@
+// import store from '@/state/store'
+
+export default [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import("../views/pages/account/login.vue"),
+    meta: {
+      beforeResolve(routeTo, routeFrom, next) {
+        // If the user is already logged in
+        console.log("checking if user")
+        if (store.getters['auth/loggedIn']) {
+          // Redirect to the home page instead
+          next({
+            name: 'home'
+          })
+        } else {
+          // Continue to the login page
+          next()
+        }
+      },
+    },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/pages/account/register.vue'),
+    // meta: {
+    //   beforeResolve(routeTo, routeFrom, next) {
+    //     // If the user is already logged in
+    //     if (store.getters['auth/loggedIn']) {
+    //       // Redirect to the home page instead
+    //       next({
+    //         name: 'home'
+    //       })
+    //     } else {
+    //       // Continue to the login page
+    //       next()
+    //     }
+    //   },
+    // },
+  },
+  {
+    path: '/forgot-password',
+    name: 'Forgot-password',
+    component: () => import('../views/pages/account/forgot-password.vue'),
+    // meta: {
+    //   beforeResolve(routeTo, routeFrom, next) {
+    //     // If the user is already logged in
+    //     if (store.getters['auth/loggedIn']) {
+    //       // Redirect to the home page instead
+    //       next({
+    //         name: 'home'
+    //       })
+    //     } else {
+    //       // Continue to the login page
+    //       next()
+    //     }
+    //   },
+    // },
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    // meta: {
+    //   authRequired: true,
+    //   beforeResolve(routeTo, routeFrom, next) {
+    //     store.dispatch('auth/logOut')
+
+    //     const authRequiredOnPreviousRoute = routeFrom.matched.some(
+    //       (route) => route.push('/login')
+    //     )
+    //     // Navigate back to previous page, or home as a fallback
+    //     next(authRequiredOnPreviousRoute ? {
+    //       name: 'home'
+    //     } : {
+    //       ...routeFrom
+    //     })
+    //   },
+    // },
+  },
+  {
+    path: '/',
+    name: 'home',
+    meta: {
+      authRequired: true,
+    },
+    component: () => import('../views/pages/dashboard/index.vue')
+  }
+
+]
