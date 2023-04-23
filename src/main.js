89b0d56729f362e08974f0a659593fceee7cde7d
PiyushAtHK <piyush.soni@brightlifecare.com>
2023-04-23 16:58:56 +0530

    Added: store and router to app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './state/store'
+import router from './router'
 
-createApp(App).mount('#app')
+
+import { initFirebaseBackend } from './helpers/firebase/authUtils';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_APIKEY,
+    authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_APP_DATABASEURL,
+    projectId: import.meta.env.VITE_APP_PROJECTId,
+    storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APP_APPId,
+    measurementId: import.meta.env.VITE_APP_MEASUREMENTID
+};
+
+initFirebaseBackend(firebaseConfig);
+
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
